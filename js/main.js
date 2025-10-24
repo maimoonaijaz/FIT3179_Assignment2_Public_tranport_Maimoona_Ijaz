75a713214c9afe0e737e7feeee04a8fe5a69236c
Maimoona <162455962+maimoonaijaz@users.noreply.github.com>
2025-10-24 18:35:19 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Embed the four Vega-Lite specs you saved in /vega-lite/
+vegaEmbed('#map1', 'vega-lite/map_points.json');
+vegaEmbed('#map2', 'vega-lite/heatmap_month_line.json');
+vegaEmbed('#map3', 'vega-lite/bars_weekday_weekend.json');
+vegaEmbed('#map4', 'vega-lite/line_trend_7dma.json');
+
+
+
